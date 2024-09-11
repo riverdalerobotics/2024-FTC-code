e2f694033881651f464127cba56bd131da08185f
Galeteanu13 <nicolas.galeteanu@gmail.com>
2024-09-11 12:09:23 -0400

    Wrote teleop modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThirdRobotCode/TeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThirdRobotCode/TeleopMode.java
@@ -1,6 +1,17 @@
 package org.firstinspires.ftc.teamcode.ThirdRobotCode;
-//hello
-public class TeleopMode {
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp (name = "Nicolas Teleop", group = "Linar OpMode")
+
+public class TeleopMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    public void runOpMode(){
+
+    }
 
 }
