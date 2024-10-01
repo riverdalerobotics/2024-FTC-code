ed317d3ad36fb620a3aa09d1334c731c97189d51
FrankieZO <fzoliveira@icloud.com>
2024-10-01 12:11:37 -0400

    Starting constructor for arm subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThirdRobotCode/ArmSubsytem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThirdRobotCode/ArmSubsytem.java
@@ -1,4 +1,33 @@
 package org.firstinspires.ftc.teamcode.ThirdRobotCode;
 
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/* cos(pivot angle) = limit/arm
+*
+*/
 public class ArmSubsytem {
+
+    DcMotor armPivot;
+    DcMotor rightExtend;
+    DcMotor leftExtend;
+
+    public ArmSubsytem(DcMotor armPivot, DcMotor rightExtend, DcMotor leftExtend){
+    this.armPivot = armPivot;
+    this.leftExtend = leftExtend;
+    this.rightExtend = rightExtend;
+
+    this.leftExtend.setDirection(DcMotorSimple.Direction.FORWARD);
+    this.rightExtend.setDirection(DcMotorSimple.Direction.REVERSE); // may reverse these
+
+    // set initial position for motors
+    }
+
+    public void extendArm(double armExtendInput){
+
+    }
+    public void pivotArm(double armPivotInput, double power){// what is power for?
+
+    }
 }
