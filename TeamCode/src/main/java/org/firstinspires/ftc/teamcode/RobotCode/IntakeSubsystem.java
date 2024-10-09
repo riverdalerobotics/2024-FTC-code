1ab70229cb875ce24e987465e9320bd4cec010f4
Galeteanu13 <nicolas.galeteanu@gmail.com>
2024-10-08 20:31:24 -0400

    Changed intake for kit bot to a servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/IntakeSubsystem.java
@@ -1,19 +1,21 @@
 package org.firstinspires.ftc.teamcode.RobotCode;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class IntakeSubsystem {
 
-    DcMotor intakeMotor;
+    CRServo intakeServo;
 
-public void IntakePowerOn(){
-
-    intakeMotor.setPower(1);
-    }
+public void IntakePowerOn() {
+    intakeServo.setPower(0.5);
+}
 
 public void intakePowerOff(){
+intakeServo.setPower(0);
 
-    intakeMotor.setPower(0);
 }
 }
 
