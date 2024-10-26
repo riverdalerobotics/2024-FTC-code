67986daef08574528d5323841e6435f7ffff38eb
Galeteanu13 <nicolas.galeteanu@gmail.com>
2024-10-26 11:15:03 -0400

    Potentially completed SlideSubsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/Commands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/Commands.java
@@ -2,9 +2,17 @@ package org.firstinspires.ftc.teamcode.RobotCode;
 import org.firstinspires.ftc.teamcode.RobotCode.ArmSubsystem;
 import org.firstinspires.ftc.teamcode.RobotCode.IntakeSubsystem;
 public class Commands {
-    public static void scoreBucket(){
-        //code to score bucket
-        //TODO: Instead of 90 make the desired angle a constant in Constants.java-Nicolas
-        ArmSubsystem.moveArm(90);
+    static class teleopCommands{
+        public static void scoreBucket(){
+            //code to score bucket
+            //TODO: Instead of 90 make the desired angle a constant in Constants.java-Nicolas
+            ArmSubsystem.moveArm(90);
+        }
     }
+
+    static class autoCommands{
+
+    }
+
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/TeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCode/TeleopMode.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class TeleopMode {
     private ElapsedTime runtime = new ElapsedTime();
     public void runOpMode(){
-        Commands.scoreBucket();
+        Commands.teleopCommands.scoreBucket();
 
     }
 
