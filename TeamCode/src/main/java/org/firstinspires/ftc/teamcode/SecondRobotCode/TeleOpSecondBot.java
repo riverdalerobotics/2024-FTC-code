ce4dead6e3cd41a64e02e8c057fddfbe41b44552
Sana Shahid <sanashahid2134@gmail.com>
2024-11-22 04:19:45 -0500

    confgured servos and motors, updated field oriented code??

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SecondRobotCode/TeleOpSecondBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SecondRobotCode/TeleOpSecondBot.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode.SecondRobotCode;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -17,23 +20,22 @@ public class TeleOpSecondBot extends  LinearOpMode {
     public DcMotor motorRightF;
     public DcMotor motorRightB;
     public DcMotor motorLeftB;
-
+    BNO055IMU imu;
     private DcMotor armMotor;
 
     // arm extender ??
     private DcMotor armExtend;
 
-    private CRServo liftMotor;
-    private CRServo intakeMotor;
+    private CRServo liftServo;
+    private CRServo intakeServo;
 
     public WebcamName camera;
-
 
     ChassisSubsystem chassis;
     ArmSubsytem arm;
     IntakeSubsystem intake;
 
-    //Angus if you see this we have to chane the file to "ArmExtender" because classes have a capitalized name
+    //TODO: Angus if you see this we have to chane the file to "ArmExtender" because classes have a capitalized name
     armExtender armExtender;
 
     //TODO: code specific: configure imu, work on field centric code, copy and paste SampleMecanumDrive into ChassisSubsystem, create an auto, tune pidf for rr.
@@ -45,10 +47,24 @@ public class TeleOpSecondBot extends  LinearOpMode {
         motorRightB = hardwareMap.get(DcMotor.class, "motorRightB");
         motorLeftB = hardwareMap.get(DcMotor.class, "motorLeftB");
         armMotor =hardwareMap.get(DcMotor.class, "armMotor");
+        armExtend = hardwareMap.get(DcMotor.class, "armExtend");
+        liftServo = hardwareMap.get(CRServo.class, "liftServo");
+        intakeServo = hardwareMap.get(CRServo.class, "intake");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit= BNO055IMU.AngleUnit.DEGREES;
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+
+        imu.initialize(parameters);
+
+
 
         chassis = new ChassisSubsystem(motorLeftF, motorRightF, motorLeftB,motorRightB);
         arm = new ArmSubsytem(armMotor);
-        intake = new IntakeSubsystem (intakeMotor, liftMotor);
+        intake = new IntakeSubsystem (intakeServo, liftServo);
+        armExtender = new armExtender(armExtend);
 
         waitForStart();
         double speedPwr;
@@ -76,11 +92,11 @@ public class TeleOpSecondBot extends  LinearOpMode {
             speedPwr = -gamepad1.left_stick_y*0.3;
             strafePwr = -gamepad1.left_stick_x*0.3;
             turnPwr = -gamepad1.right_stick_x*0.3;
-            chassis.moveMechChassis(speedPwr, strafePwr, turnPwr);
+            chassis.fieldOriented(0,speedPwr, strafePwr, turnPwr);
             slidePwrTemp = -gamepad2.right_stick_y;
             intakePwr = 0.2;
             wristPwr = 0.3;
-
+            //imu.
             armExtender.armExt(slidePwrTemp);
 
 
