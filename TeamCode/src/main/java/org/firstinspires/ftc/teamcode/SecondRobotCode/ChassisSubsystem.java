71fb6c79952357c788d3c453f2c68a0e833070d9
BrandonR3P2 <branho19800@gmail.com>
2025-01-15 22:40:52 -0500

    changed driver controls so that you don't need pre intake on seperate control (bot 2)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SecondRobotCode/ChassisSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SecondRobotCode/ChassisSubsystem.java
@@ -77,7 +77,7 @@ public class ChassisSubsystem extends MecanumDrive{
     private List<Integer> lastEncVels = new ArrayList<>();
 
 
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(4.5, 1.5, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(3.5, 1.5, 0);
     public static PIDCoefficients HEADING_PID = new PIDCoefficients(0.7, 3, 0);
 
     public static double LATERAL_MULTIPLIER = 1.052; //1.48 //1.336
